a3ed168968b4574033cab984aa16ee46e5190100
Alxplcn <139807383+Alxplcn@users.noreply.github.com>
2023-07-17 22:03:16 +0300

    Create step_4.py

---
+++ first_lesson/step_4.py
@@ -0,0 +1,18 @@
+import requests
+
+'''Функция принимает наименование локации, по которой нужно получить погоду, и возвращает
+полученный от сервера результат. В случае, если сервер вернул ошибку, возвращается слово
+"Ошибка"
+'''
+def get_weather_from_certain_location(location: str) -> str:
+  base_url = 'https://wttr.in/'
+  response = requests.get(base_url+location)
+  try:
+    response.raise_for_status()
+  except requests.exceptions.HTTPError:
+    return 'Ошибка'
+  return response.text
+  
+print(get_weather_from_certain_location('london'))
+print(get_weather_from_certain_location('svo'))
+print(get_weather_from_certain_location('Череповец'))
