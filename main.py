9022b1167573f799234eb911b3222a893de6c310
Alxplcn <139807383+Alxplcn@users.noreply.github.com>
2023-07-23 14:06:53 +0300

    Исправления в связи с последними требованиями

--- main.py
+++ main.py
@@ -1,48 +1,26 @@
 import requests
 import os
 
-def get_weather(location: str) -> str:
+
+def get_weather(location: str, payload: dict) -> str:
     """Функция принимает наименование локации, по которой нужно получить погоду, и возвращает
     полученный от сервера результат. В случае, если сервер вернул ошибку, возвращается слово
     "Ошибка"
     """
     base_url = 'https://wttr.in/'
-    response = requests.get(f"{base_url} {location}")
+    response = requests.get(f"{base_url} {location}", params=payload)
+    response.raise_for_status()
     return response.text
 
 
 def main():
     os.system('color')
-    # Шаг 3
-    payload = {'nTqu': '',
-               'lang': 'en'}
-    url = 'https://wttr.in/san%20francisco'
-    response = requests.get(url, params=payload)
-    try:
-        response.raise_for_status()
-        print(response.text)
-    except requests.exceptions.HTTPError:
-        print('Ошибка на шаге 3')
-
-    # Шаг 4
-    locations = ['london', 'svo', 'Череповец']
-    for location in locations:
-        try:
-            print(get_weather(location))
-        except requests.exceptions.HTTPError:
-            print('Ошибка на шаге 4')
-
-    # Шаг 5
     payload = {'MmnqT': '',
                'lang': 'ru'}
-    url = 'https://wttr.in/Череповец'
-    response = requests.get(url, params=payload)
-    try:
-        response.raise_for_status()
-        print(response.text)
-    except requests.exceptions.HTTPError:
-        print('Ошибка на шаге 5')
 
+    locations = ['london', 'svo', 'Череповец']
+    for location in locations:
+        print(get_weather(location, payload))
     input("Нажмите любую клавишу для выхода ")
 
 
